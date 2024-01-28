c9910db66f006cd3bd18b4b55e20a54b5e3a4533
Akshay Bhinge <87247305+akshaybhinge19@users.noreply.github.com>
2024-01-28 23:12:00 +0530

    formatting changes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,64 +1,67 @@
 <template>
-    <header id="header" class="bg-gray-700">
-        <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-            <!-- App Name -->
-            <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{ name: 'home' }"
-                exact-active-class="no-active">Music</router-link>
+  <header id="header" class="bg-gray-700">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <!-- App Name -->
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
-            <div class="flex flex-grow items-center">
-                <!-- Primary Navigation -->
-                <ul class="flex flex-row mt-1">
-                    <!-- Navigation Links -->
-                    <li>
-                        <router-link class="text-white px-2" :to="{ name: 'about' }">About</router-link>
-                    </li>
-                    <li v-if="!userStore.userLoggedIn">
-                        <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login /
-                            Register</a>
-                    </li>
-                    <template v-else>
-                        <li>
-                            <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
-                        </li>
-                        <li>
-                            <a class="px-2 text-white" href="#" @click.prevent="handleLogout">Logout</a>
-                        </li>
-                    </template>
-                </ul>
-            </div>
-        </nav>
-    </header>
+      <div class="flex flex-grow items-center">
+        <!-- Primary Navigation -->
+        <ul class="flex flex-row mt-1">
+          <!-- Navigation Links -->
+          <li>
+            <router-link class="text-white px-2" :to="{ name: 'about' }">About</router-link>
+          </li>
+          <li v-if="!userStore.userLoggedIn">
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
+          </li>
+          <template v-else>
+            <li>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
+            </li>
+            <li>
+              <a class="px-2 text-white" href="#" @click.prevent="handleLogout">Logout</a>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
-
 
 <script>
 import { mapStores, mapActions } from 'pinia'
 import useModalStore from '@/stores/modal'
 import useUserStore from '@/stores/user'
 export default {
-    name: 'AppHeader',
-    computed: {
-        ...mapStores(useModalStore, useUserStore)
+  name: 'AppHeader',
+  computed: {
+    ...mapStores(useModalStore, useUserStore)
+  },
+  methods: {
+    ...mapActions(useModalStore, {
+      signOut: 'signOut'
+    }),
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen
     },
-    methods: {
-        ...mapActions(useModalStore, {
-            signOut: "signOut"
-        }),
-        toggleAuthModal() {
-            this.modalStore.isOpen = !this.modalStore.isOpen;
-        },
-        async handleLogout() {
-            try {
-                await this.userStore.signOut();
-                console.log("signout", this.$route)
-                if (this.$route.meta.requiresAuth) {
-                    this.$router.push({ name: 'home' });
-                }
-            } catch (error) {
-                console.error("Error occured while logging out.");
-            }
+    async handleLogout() {
+      try {
+        await this.userStore.signOut()
+        console.log('signout', this.$route)
+        if (this.$route.meta.requiresAuth) {
+          this.$router.push({ name: 'home' })
         }
+      } catch (error) {
+        console.error('Error occured while logging out.')
+      }
     }
+  }
 }
-
 </script>
